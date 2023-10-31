5cd130148f65dca2f654f355a9a1ec15dd102cb3
iKrNitin <cksnitin77@gmail.com>
2023-10-31 11:08:39 +0530

    added author and main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # guessTheNumber-game1
 This is my Game 1 
+Author - Nitin Kumar
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>;
+using namespace std;
+
+int main(){
+    cout<<"this is first c++ file"<<endl;
+    cout<<"this is the another text"<<endl;
+}
